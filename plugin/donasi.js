522dbfda63bfa5fdd3e12c7878faabda46ad9293
Alfarabotz567 <109883756+Alfarabotz567@users.noreply.github.com>
2022-12-05 05:07:46 +0700

    Create donasi.js

---
+++ plugin/donasi.js
@@ -0,0 +1,12 @@
+let handler = async m => m.reply(`
+╭─「 Donasi • Pulsa 」
+│ •  [0816-4682-9895]
+│   「 Donasi • Dana」
+│ •  [0895-3170-4997]
+╰────
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
